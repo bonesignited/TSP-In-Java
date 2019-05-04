7da727206bc4f66f99b6ff4313e10f87aad44192
bonesignited <zlxexit@gmail.com>
2019-05-04 19:25:23 +0800

    初始化项目

---
+++ src/main/java/com/zhoulx/gavisualization/GaVisualizationApplication.java
@@ -0,0 +1,13 @@
+package com.zhoulx.gavisualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaVisualizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaVisualizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhoulx/gavisualization/GaVisualizationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhoulx.gavisualization;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GaVisualizationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
